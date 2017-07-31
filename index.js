b51e4b603ba201cc93e2f04fc249a1faf7318f1a
Finn Ellis <relsqui@chiliahedron.com>
2017-07-30 22:49:33 -0700

    Implement navigation!

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import Koa from "koa";
-import Router from "koa-router";
-import bodyParser from "koa-bodyparser";
+import Koa from 'koa';
+import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
 
-import * as secrets from "./secrets";
-import {locations} from "./map";
+import * as secrets from './secrets';
+import * as map from './map';
 
 const app = new Koa();
 const router = new Router();
@@ -12,43 +12,23 @@ app.use(bodyParser());
 
 var players = {};
 
-router.post("/", async (ctx) => {
+router.post('/', async (ctx) => {
     if (ctx.request.body.token != secrets.VER_TOKEN) {
         ctx.response.status = 403;
         return;
     }
     ctx.response.status = 200;
+
     var id = ctx.request.body.user_id;
     if (!(id in players)) {
         players[id] = {
-            "loc": "center",
+            'loc': 'center',
         }
     }
-
-    var user = players[id];
-    var loc = locations[user.loc];
-    ctx.response.body =  {
-        "attachments": [{
-            "title": loc.name,
-            "text": loc.desc,
-            "callback_id": "button_press",
-            "actions": [
-                {
-                    "name": "one",
-                    "type": "button",
-                    "text": "one"
-                },
-                {
-                    "name": "two",
-                    "type": "button",
-                    "text": "two"
-                }
-            ]
-        }]
-    };
+    build_response(ctx, players[id]);
 });
 
-router.post("/button", async (ctx) => {
+router.post('/button', async (ctx) => {
     // Bodyparse doesn't parse this for us because it's a text field inside
     // the JSON response object, it just happens to contain more JSON.
     var payload = JSON.parse(ctx.request.body.payload);
@@ -57,15 +37,17 @@ router.post("/button", async (ctx) => {
         return;
     }
     ctx.response.status = 200;
-    switch(payload.actions[0].name) {
-        case "one":
-            ctx.response.body = ":one:";
-            break;
-        case "two":
-            ctx.response.body = ":two:";
-            break;
-    }
+    //
+    // By the time they get here they should definitely exist.
+    var player = players[payload.user.id];
+    player.loc = payload.actions[0].name;
+    build_response(ctx, player);
 });
+
+function build_response(ctx, player) {
+    ctx.response.body = {};
+    ctx.response.body.attachments = [map.build_attachment_for(player)]
+}
 
 app.use(router.routes());
 app.listen(8080);
--- map.js
+++ map.js
@@ -1,46 +1,67 @@
-export var locations = {
-    "center": {
-        "name": "A Good Starting Place",
-        "desc": "At least, one hopes it is, because this is all there is.",
-        "exits": {
-            "North": "forest",
-            "East": "beach",
-            "Down": "hole"
+export function build_attachment(loc) {
+    var attachment = {
+        'title': loc.name,
+        'text': loc.desc,
+        'callback_id': 'button_press',
+        'actions': []
+    }
+    for (var exit_label in loc.exits) {
+        attachment.actions.push({
+            'type': 'button',
+            'name': loc.exits[exit_label],
+            'text': exit_label,
+        });
+    }
+    return attachment;
+}
+
+export function build_attachment_for(player) {
+    return build_attachment(locations[player.loc]);
+}
+
+var locations = {
+    'center': {
+        'name': 'A Good Starting Place',
+        'desc': 'At least, one hopes it is, because this is all there is.',
+        'exits': {
+            'North': 'forest',
+            'East': 'beach',
+            'Down': 'hole'
         }
     },
 
-    "forest": {
-        "name": "The Forest",
-        "desc": "It\"s cool, shady, and green.",
-        "exits": {
-            "South": "center"
+    'forest': {
+        'name': 'The Forest',
+        'desc': 'It\'s cool, shady, and green.',
+        'exits': {
+            'South': 'center'
         }
     },
 
-    "beach": {
-        "name": "On a Beach",
-        "desc": "It\"s very warm here and the sand crunches pleasantly underfoot.",
-        "exits": {
-            "West": "center"
+    'beach': {
+        'name': 'On a Beach',
+        'desc': 'It\'s very warm here and the sand crunches pleasantly underfoot.',
+        'exits': {
+            'West': 'center'
         }
     },
 
-    "hole": {
-        "name": "In a Hole",
-        "desc": "It\"s pretty unpleasant down here. Luckily, there\"s a ladder.",
-        "exits": {
-            "Up": "center"
+    'hole': {
+        'name': 'In a Hole',
+        'desc': 'It\'s pretty unpleasant down here. Luckily, there\'s a ladder.',
+        'exits': {
+            'Up': 'center'
         }
     }
 }
 
 var template = {
 
-    "id": {
-        "name": "Name",
-        "desc": "A long description with sentences.",
-        "exits": {
-            "Label": "destination-id"
+    'id': {
+        'name': 'Name',
+        'desc': 'A long description with sentences.',
+        'exits': {
+            'Label': 'destination-id'
         }
     }
 
